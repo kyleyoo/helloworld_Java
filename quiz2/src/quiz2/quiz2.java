fc296c968ea803e00ebd20741d6107e6389b61ea
유희상 <gogohs0116@naver.com>
2017-01-16 10:53:56 +0900

    dasdasdd

---
+++ quiz2/src/quiz2/quiz2.java
@@ -0,0 +1,55 @@
+package quiz2;
+
+import java.util.Scanner;
+
+public class quiz2 {
+
+	public static void main(String[] args) {
+
+		int a;
+		String b;
+		b="a";
+		
+
+		Scanner sc = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+
+		do {
+			System.out.println("숫자를 입력하세요");
+			a = sc.nextInt();
+			System.out.println(a);
+			if (a == 1) {
+				System.out.println("*");
+				System.out.println("**");
+				System.out.println("***");
+				System.out.println("****");
+
+			} else if (a == 2) {
+				System.out.println("   *");
+				System.out.println("  **");
+				System.out.println(" ***");
+				System.out.println("****");
+				System.out.println("Sdsdsds");
+			} else if (a == 3) {
+				System.out.println("****");
+				System.out.println(" ***");
+				System.out.println("  **");
+				System.out.println("   *");
+
+			} else if (a == 4) {
+				System.out.println("****");
+				System.out.println("***");
+				System.out.println("**");
+				System.out.println("*");
+
+			}
+			System.out.println("더하려면 d 그만하려면 y");
+		
+		} 
+		
+		while (!sc2.nextLine().equals("y"));
+		System.out.println();
+		System.out.println("프로그램 종료");
+	}
+
+}
